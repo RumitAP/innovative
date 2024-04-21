5b53fd00d6ecd01242aaafc06fa8a65f7b1679d3
RumitAP <rumitp97@gmail.com>
2024-04-21 16:43:06 -0400

    back end models completed with relationships

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # innovative
 Innovative Refrigeration Systems Challenge
+
+
+Remember to set :
+
+* `export FLASK_APP=app`
+
+* when you do `pip install -r requirements.txt` and any modules are not found when running the app, make sure your python path is pointing to the virtualenv
+
+* Make Migrations: `flask db migrate -m "Your message about what this migration does"`
+
+* Run migrations: `flask db upgrade`
--- app/models.py
+++ app/models.py
@@ -1,4 +1,75 @@
 from app import db
 
-class JobHazardAnalysis(db.model):
-    pass
+class JobHazardAnalysisPreventativeMeasure(db.Model):
+    # One to one with Hazard
+    __tablename__ = 'job_hazard_analysis_task_hazard_preventative_measure'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    job_harazrd_analysis_task_hazard_id = db.Column(db.Integer, db.ForeignKey('job_hazard_analysis_task_hazard.id'), nullable=False, index=True)
+    description = db.Column(db.String, nullable=False)
+    
+    hazard = db.relationship('JobHazardAnalysisTaskHazard', back_populates='preventative_measure', uselist=False)
+
+class JobHazardAnalysisTaskConsequences(db.Model):
+    # One to many with Hazard
+    __tablename__ = 'job_hazard_analysis_task_consequence'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    job_harazrd_analysis_task_hazard_id = db.Column(db.Integer, db.ForeignKey('job_hazard_analysis_task_hazard.id'), nullable=False, index=True)
+    description = db.Column(db.String, nullable=False)
+    
+    hazard = db.relationship('JobHazardAnalysisTaskHazard', back_populates='consequences')
+
+class JobHazardAnalysisTaskHazard(db.Model):
+    # One to many with Task
+    __tablename__ = 'job_hazard_analysis_task_hazard'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    job_harazrd_analysis_task_id = db.Column(db.Integer, db.ForeignKey('job_hazard_analysis_task.id'), nullable=False, index=True)
+    description = db.Column(db.String, nullable=False)
+    
+    task = db.relationship('JobHazardAnalysisTask', back_populates='hazards')
+    consequences = db.relationship('JobHazardAnalysisTaskConsequences', order_by=JobHazardAnalysisTaskConsequences.id, back_populates='hazard')
+    preventative_measure = db.relationship('JobHazardAnalysisPreventativeMeasure', back_populates='hazard', uselist=False)
+
+class JobHazardAnalysisTask(db.Model):
+    # One to many with JHA
+    __tablename__ = 'job_hazard_analysis_task'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    job_hazard_analysis_id = db.Column(db.Integer, db.ForeignKey('job_hazard_analysis.id'), nullable=False, index=True)
+    task_description = db.Column(db.String, nullable=False)
+    step = db.Column(db.Integer, nullable=False)
+    
+    job_hazard_analysis = db.relationship('JobHazardAnalysis', back_populates='tasks')
+    hazards = db.relationship('JobHazardAnalysisTaskHazard', order_by=JobHazardAnalysisTaskHazard.id, back_populates='task')
+    
+    
+    def __init__(self, **kwargs):
+        super(JobHazardAnalysisTask, self).__init__(**kwargs)
+        self.set_step()
+
+    def set_step(self):
+        # Added step because a JHA has a "series" of steps. Assuming they are completed in order.
+        # Find the current max step for the associated JobHazardAnalysis
+        if self.job_hazard_analysis_id:
+            max_step = db.session.query(db.func.max(JobHazardAnalysisTask.step)).filter_by(
+                job_hazard_analysis_id=self.job_hazard_analysis_id
+            ).scalar()
+            self.step = (max_step or 0) + 1
+
+    def __repr__(self):
+        return f'<JobHazardAnalysisTask {self.id}>'
+
+
+class JobHazardAnalysis(db.Model):
+    __tablename__ = 'job_hazard_analysis'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(128), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    
+    tasks = db.relationship('JobHazardAnalysisTask', order_by=JobHazardAnalysisTask.id, back_populates='job_hazard_analysis')
+
+    def __repr__(self):
+        return f'<JobHazardAnalysis {self.id}>'
